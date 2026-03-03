f8fc18be545c40419884286fb3e57010ef454a48
PyTaskQ <test@example.com>
2026-03-03 11:43:11 -0700

    feat: auto-commit agent changes

--- pyshort/__init__.py
+++ pyshort/__init__.py
@@ -2,4 +2,22 @@
 pyshort - A simple URL shortener application
 """
 
+from .stats import (
+	StatsTracker,
+	get_global_tracker,
+	track_click,
+	get_click_count,
+	get_daily_stats,
+	get_top_urls,
+)
+
 __version__ = "0.1.0"
+
+__all__ = [
+	"StatsTracker",
+	"get_global_tracker",
+	"track_click",
+	"get_click_count",
+	"get_daily_stats",
+	"get_top_urls",
+]
